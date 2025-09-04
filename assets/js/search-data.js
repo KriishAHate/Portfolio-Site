96e87088a4cdf49aa602fd8259460b09e16bf313
KriishAHate <100579261+KriishAHate@users.noreply.github.com>
2025-09-04 02:31:47 +0000

    Deploying to gh-pages from @ KriishAHate/Portfolio-Site@b63612a2779ede6af430f026cf017c0821ae83fa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -436,21 +436,6 @@ ninja.data = [{
           description: "Turning a fallen tree stump into functional furniture with a repeatable, optimized design process.",
           section: "Projects",handler: () => {
               window.location.href = "/Portfolio-Site/projects/woodworking/";
-            },},{id: "projects-yamaha-rx-135-restoration-amp-performance-refresh",
-          title: 'Yamaha RX 135 — Restoration &amp;amp; Performance Refresh',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/Portfolio-Site/projects/rx135/";
-            },},{id: "projects-hero-honda-cbz-revival-amp-refresh",
-          title: 'Hero Honda CBZ — Revival &amp;amp; Refresh',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/Portfolio-Site/projects/cbz/";
-            },},{id: "projects-project-title",
-          title: 'PROJECT TITLE',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/Portfolio-Site/projects/%3Cslug%3E/";
             },},{
         id: 'social-email',
         title: 'email',
