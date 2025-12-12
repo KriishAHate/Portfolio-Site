446c397531210325a63d4dd7b5445c88bf9aa43d
KriishAHate <100579261+KriishAHate@users.noreply.github.com>
2025-12-12 23:17:39 +0000

    Deploying to gh-pages from @ KriishAHate/Portfolio-Site@55aa4dba82ff4efcb6fba98a14321412a334c583 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -457,7 +457,7 @@ ninja.data = [{
         title: 'YouTube',
         section: 'Socials',
         handler: () => {
-          window.open("https://youtube.com/@youtube.com/@kriishhate", "_blank");
+          window.open("https://youtube.com/@youtube.com", "_blank");
         },
       },{
       id: 'light-theme',
